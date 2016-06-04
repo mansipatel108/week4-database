f60024df52b2aee20a945914b6ad6d5c24799439
Mansi <mpatel.y@gmail.com>
2016-06-04 18:12:21 -0400

    added gridview in students page

--- week4/Students.aspx.cs
+++ week4/Students.aspx.cs
@@ -5,12 +5,44 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//using statements are required to connect EF database
+using week4.Models;
+using System.Web.ModelBinding;
+
 namespace week4
 {
     public partial class Students : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if loading the page for thhe first time populate the student grid
+            if (!IsPostBack)
+            {
+                //get the students data
+                this.GetStudents();
+            }
+        }
+        /**
+         * <summery>
+         * This method gets the students data from the DB
+         * </summery>
+         * 
+         * @method GetStudents
+         * @return {void}
+         * */
+        protected void GetStudents()
+        {
+            //connect to EF
+            using(DefaultConnection db = new DefaultConnection())
+            {
+                //query students table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                //bind the result in GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
 
         }
     }
