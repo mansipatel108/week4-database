7bc7256353f7b833a3a52c9d42ba227329f54ab8
Mansi <mpatel.y@gmail.com>
2016-06-04 15:27:29 -0400

    Add project files.

---
+++ COMP2007-Lesson4B/Students.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Students" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="COMP2007_Lesson4B.WebForm1" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView runat="server" ID="StudentsGridView"></asp:GridView>
+</asp:Content>
---
+++ COMP2007-Lesson4B/Students.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//require for EF db
+using COMP2007_Lesson4B.Models;
+using System.Web.ModelBinding;
+namespace COMP2007_Lesson4B
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if loading the page for first time, populae the grid fro EF Db
+            if(IsPostBack)
+            {
+                //get data
+                this.GetStudents();
+            }
+
+        }
+        protected void GetStudents()
+        {
+            //connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //query the students table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+                //bind thhe result to the gridview
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+        }
+    }
+}
